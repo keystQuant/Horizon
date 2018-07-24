54e34cd1e182cbadc89c7c933ab57e8e7865712d
1 <ppark9553@gmail.com>
2018-07-24 15:21:59 +0900

    기본 페이지 완성

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-// 아래 부분을 지우면 몽고DB 사용 없이 외부 API 연결 작업을 한다
-const mongoose = require('mongoose');
-
-const Test = require('./src/models/test');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('assets')); // CSS 파일 연결
@@ -29,46 +24,6 @@ const stopServer = () => {
   // 테스트할 때 필요한 툴 (프로덕션 상에서는 상관없음)
 };
 
-// [ CONFIGURE mongoose ]
-
-// CONNECT TO MONGODB SERVER
-const db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', () => {
-  // CONNECTED TO MONGODB SERVER
-  console.log('몽고DB 연결하였습니다.');
-});
-
-// connect to localhost if not in Docker container
-const mongoURL = process.env.ENVIRONMENT === 'docker'
-  ? 'mongodb://horizonadmin:makeitpopwe123HORIZON@mongo:27017/horizon?authSource=admin' // ENVIRONMENT 환경변수가 도커면 도커 컨테이너로 연결
-  : 'mongodb://horizonadmin:makeitpopwe123HORIZON@localhost:27017/horizon?authSource=admin'; // 그렇지 않다면 개발 환경 (로컬로 연결)
-mongoose.connect(mongoURL);
-
-// //////////////////////// -->>> API HERE
-// TEST API
-app.get('/', (req, res) => {
-  res.send('서버가 제대로 작동합니다!');
-});
-
-app.post('/api/test', (req, res) => {
-  // POST할 때 받은 데이터값을 몽고디비로 보내서 저장한다
-  // 저장에 성공하면 result가 1, 실패하면 0이다
-  const testInst = new Test();
-  testInst.author = req.body.author; // author와 content를 post data로 받음
-  testInst.content = req.body.content;
-
-  testInst.save((error) => {
-    if (error) {
-      console.log(error);
-      res.json({ result: 0 });
-    }
-
-    res.json({ result: 1 });
-  });
-});
-
-
 // //////////////////////// //
 // // URL 정의는 여기서 부터 // //
 // /////////////////////// //
@@ -76,6 +31,6 @@ app.post('/api/test', (req, res) => {
 // 여기부터는 프론트엔드 개발자의 창의력을 보여주세요~! //
 
 // Horizon 홈 페이지
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   res.render('home.html');
 });
--- src/home.js
+++ src/home.js
@@ -1,3 +1,27 @@
-const helloWords = 'hi there';
-
-console.log(helloWords);
+document.addEventListener('click', async (e) => {
+  if (e.target.id === 'rancher') {
+    window.location.href = 'http://149.28.25.177:8080/';
+  } else if (e.target.id === 'munin') {
+    console.log('Munin server');
+  } else if (e.target.id === 'trello') {
+    window.location.href = 'https://trello.com/b/r3mANzdO/%ED%82%A4%EC%8A%A4%ED%86%A4';
+  } else if (e.target.id === 'vultr') {
+    window.location.href = 'https://my.vultr.com/';
+  } else if (e.target.id === 'github') {
+    window.location.href = 'https://github.com/keystQuant';
+  } else if (e.target.id === 'travis') {
+    window.location.href = 'https://travis-ci.org/';
+  } else if (e.target.id === 'sentry') {
+    window.location.href = 'https://sentry.io/keystone-investments/';
+  } else if (e.target.id === 'apiary') {
+    window.location.href = 'https://app.apiary.io/buzzz/editor';
+  } else if (e.target.id === 'django') {
+    console.log('Django admin');
+  } else if (e.target.id === 'analytics') {
+    window.location.href = 'https://www.google.com/analytics/';
+  } else if (e.target.id === 'drive') {
+    window.location.href = 'https://www.google.com/drive/';
+  } else if (e.target.id === 'blog') {
+    window.location.href = 'https://blog.naver.com/ppark9553';
+  }
+});
